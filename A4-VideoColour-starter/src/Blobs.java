1e7441cb475455a788483c311ea9732b59edd770
Ritsuki Otsuka <y.tsuka0728@icloud.com>
2020-04-03 23:55:59 -0300

    Submission Updated

--- A4-VideoColour-starter/src/Blobs.java
+++ A4-VideoColour-starter/src/Blobs.java
@@ -2,14 +2,14 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Blobs extends ArrayList implements Blob{
+public class Blobs implements Blob {
 
     ArrayList<Point> blobArray;
     private int row;
     private int col;
     private int label;
     private int color;
-    private final int [] palette = {8956291, 13512253, 15722670, 15896621, 12515920, 16440395, 8550440, 8563325, 15762040, 15787690};
+    private final int[] palette = {8956291, 13512253, 15722670, 15896621, 12515920, 16440395, 8550440, 8563325, 15762040, 15787690};
     private static int paletteCounter = 0;
 
     public Blobs() {
--- A4-VideoColour-starter/src/DepthImageProcessor.java
+++ A4-VideoColour-starter/src/DepthImageProcessor.java
@@ -6,13 +6,13 @@ import java.util.*;
 /**
  * Complete this class as part of the assignment
  */
-public class DepthImageProcessor implements ImageProcessor{
+public class DepthImageProcessor implements ImageProcessor {
     private double currentThreshold;
     private double minDistance;
     private double maxDistance;
     private int width;
     private int height;
-    private double [][] imageArray;
+    private double[][] imageArray;
     private int[][] colorImg;
     private Blobs[][] blobs;
     private Blobs[][] newBlobs;
@@ -30,15 +30,15 @@ public class DepthImageProcessor implements ImageProcessor{
             height = fileScanner.nextInt();
 
             imageArray = new double[height][width];
-            //if(colorImg == null)
+            if(colorImg == null)
                 colorImg = new int[height][width];
 
-            if(blobs == null)
+            if (blobs == null)
                 blobs = new Blobs[height][width];
 
             blobArray.clear();
 
-            for(int i = 0; i < height; i++) {
+            for (int i = 0; i < height; i++) {
                 for (int j = 0; j < width; j++) {
                     imageArray[i][j] = fileScanner.nextDouble();
                 }
@@ -55,51 +55,43 @@ public class DepthImageProcessor implements ImageProcessor{
     /**
      * Helper method to create an array of colorImg
      */
-    private void setColorImg()
-    {
-        boolean [][] visited = new boolean[height][width];
-        int[][] newColorImg = new int[height][width];
+    private void setColorImg() {
+        boolean[][] visited = new boolean[height][width];
         newBlobs = new Blobs[height][width];
 
-        for(boolean[] a : visited)
+        for (boolean[] a : visited)
             Arrays.fill(a, false);
 
-        for(int i = 0; i < height; i++)
-        {
-            for(int j = 0; j < width; j++)
-            {
-                if(imageArray[i][j] > maxDistance || imageArray[i][j] < minDistance) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (imageArray[i][j] > maxDistance || imageArray[i][j] < minDistance) {
                     visited[i][j] = true;
                     newBlobs[i][j] = null;
                     colorImg[i][j] = 0;
-                }
-                else if (!visited[i][j]) {
-                    BFS(newColorImg, visited, i, j);
-                    //colorImg[i][j] = 1000000000;
+                } else if (!visited[i][j]) {
+                    BFS(visited, i, j);
                 }
             }
         }
 
-        for(int i = 0; i < height; i++) {
-            for(int j = 0; j < width; j++) {
-                if(newBlobs[i][j] != null)
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (newBlobs[i][j] != null)
                     colorImg[i][j] = newBlobs[i][j].getColor();
             }
         }
 
         blobs = newBlobs;
-        //colorImg = newColorImg;
     }
 
     /**
      * Helper method to use breath first search to find a blob area being assigned as the same color
      *
      * @param visited an array representing which pixels has been checked so far
-     * @param r a row at which bfs starts
-     * @param c a column bfs starts
+     * @param r       a row at which bfs starts
+     * @param c       a column bfs starts
      */
-    private void BFS(int[][] newColorImg ,boolean[][] visited, int r, int c)
-    {
+    private void BFS(boolean[][] visited, int r, int c) {
         Queue<Point> searchQueue = new ArrayDeque<>();
         searchQueue.add(new Point(r, c));
         int rowSum = 0;
@@ -108,8 +100,7 @@ public class DepthImageProcessor implements ImageProcessor{
 
         Blobs blob = new Blobs();
 
-        while(!searchQueue.isEmpty())
-        {
+        while (!searchQueue.isEmpty()) {
             counter++;
             Point p = searchQueue.poll();
             int row = p.x;
@@ -121,56 +112,56 @@ public class DepthImageProcessor implements ImageProcessor{
             newBlobs[row][col] = blob;
 
             //left
-            if(isValidMove(row - 1, col)) {
+            if (isValidMove(row - 1, col)) {
                 if (!visited[row - 1][col] && isWithinThreshold(row, col, row - 1, col)) {
                     visited[row - 1][col] = true;
                     searchQueue.add(new Point(row - 1, col));
                 }
             }
             //right
-            if(isValidMove(row + 1, col)) {
+            if (isValidMove(row + 1, col)) {
                 if (!visited[row + 1][col] && isWithinThreshold(row, col, row + 1, col)) {
                     visited[row + 1][col] = true;
                     searchQueue.add(new Point(row + 1, col));
                 }
             }
             //up
-            if(isValidMove(row, col - 1)) {
+            if (isValidMove(row, col - 1)) {
                 if (!visited[row][col - 1] && isWithinThreshold(row, col, row, col - 1)) {
                     visited[row][col - 1] = true;
                     searchQueue.add(new Point(row, col - 1));
                 }
             }
             //down
-            if(isValidMove(row, col + 1)) {
+            if (isValidMove(row, col + 1)) {
                 if (!visited[row][col + 1] && isWithinThreshold(row, col, row, col + 1)) {
                     visited[row][col + 1] = true;
                     searchQueue.add(new Point(row, col + 1));
                 }
             }
             //upper left
-            if(isValidMove(row - 1, col - 1)) {
+            if (isValidMove(row - 1, col - 1)) {
                 if (!visited[row - 1][col - 1] && isWithinThreshold(row, col, row - 1, col - 1)) {
                     visited[row - 1][col - 1] = true;
                     searchQueue.add(new Point(row - 1, col - 1));
                 }
             }
             //upper right
-            if(isValidMove(row - 1, col + 1)) {
+            if (isValidMove(row - 1, col + 1)) {
                 if (!visited[row - 1][col + 1] && isWithinThreshold(row, col, row - 1, col + 1)) {
                     visited[row - 1][col + 1] = true;
                     searchQueue.add(new Point(row - 1, col + 1));
                 }
             }
             //lower left
-            if(isValidMove(row + 1, col - 1)) {
+            if (isValidMove(row + 1, col - 1)) {
                 if (!visited[row + 1][col - 1] && isWithinThreshold(row, col, row + 1, col - 1)) {
                     visited[row + 1][col - 1] = true;
                     searchQueue.add(new Point(row + 1, col - 1));
                 }
             }
             //lowe right
-            if(isValidMove(row + 1, col + 1)) {
+            if (isValidMove(row + 1, col + 1)) {
                 if (!visited[row + 1][col + 1] && isWithinThreshold(row, col, row + 1, col + 1)) {
                     visited[row + 1][col + 1] = true;
                     searchQueue.add(new Point(row + 1, col + 1));
@@ -184,7 +175,7 @@ public class DepthImageProcessor implements ImageProcessor{
         int colAve = colSum / counter;
         blob.setCentroid(rowAve, colAve);
 
-        if(blobs[rowAve][colAve] != null && blobs[rowAve][colAve].getColor() != 0) {
+        if (blobs[rowAve][colAve] != null && blobs[rowAve][colAve].getColor() != 0) {
             blob.setLabel(blobs[rowAve][colAve].getLabel());
             blob.setColor(blobs[rowAve][colAve].getColor());
             blobs[rowAve][colAve].setColor(0);
@@ -193,6 +184,7 @@ public class DepthImageProcessor implements ImageProcessor{
         blobArray.add(blob);
 
     }
+
     /**
      * Helper method to check the given row and col are valid
      *
@@ -200,8 +192,7 @@ public class DepthImageProcessor implements ImageProcessor{
      * @param col
      * @return true if it is a valid pixel
      */
-    private boolean isValidMove(int row, int col)
-    {
+    private boolean isValidMove(int row, int col) {
         return row >= 0 && row < height && col >= 0 && col < width;
     }
 
@@ -214,8 +205,7 @@ public class DepthImageProcessor implements ImageProcessor{
      * @param c2 a neighboring column
      * @return true if the distance of the two pixels are within the threshold
      */
-    private boolean isWithinThreshold(int r1, int c1, int r2, int c2)
-    {
+    private boolean isWithinThreshold(int r1, int c1, int r2, int c2) {
         return Math.abs(imageArray[r1][c1] - imageArray[r2][c2]) < currentThreshold;
     }
 
@@ -256,6 +246,6 @@ public class DepthImageProcessor implements ImageProcessor{
 
     @Override
     public void setMaxDist(double maxDist) {
-        maxDistance =  maxDist;
+        maxDistance = maxDist;
     }
 }
